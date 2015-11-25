35086ac7c7fae3ef8cbc5e8ba3ca55315525c184
IoPhoenix <olga.fomin26@gmail.com>
2015-11-24 22:12:44 -0800

    added functionality for tutorial page

--- gamePrototype/javascript/tutorial.js
+++ gamePrototype/javascript/tutorial.js
@@ -1,2 +1,20 @@
-// functionality for the tutorial page
-// in progress
+// initialize mouse over tooltips
+$( document ).ready(function() { 
+	$( document ).tooltip();
+ });
+
+// disable mouse over tooltips if the page is not tutorial
+$(document).on("pagecontainerbeforeshow",function(event, ui) {
+    var activePage = ui.toPage;
+	// var activePage = $('body').pagecontainer('getActivePage');
+    var activePageId = activePage[0].id;
+    if(activePageId !== "tutorial-page") {
+ 	   $( "#user-points, #quit-game, #open-backpack, #hints-btn, #penguins").tooltip({
+  			disabled: true
+		});
+	} else if (activePageId == "tutorial-page") {
+		$( "#user-points, #quit-game, #open-backpack, #hints-btn, #penguins").tooltip({
+  			disabled: false
+		});
+	}
+  });
